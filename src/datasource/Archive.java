b089b703e9ecda1c0c68b10ec9409e50bd9026cf
NielsB <niller-niller@hotmail.com>
2018-10-10 11:08:06 +0200

    Archive format

--- src/datasource/Archive.java
+++ src/datasource/Archive.java
@@ -6,29 +6,27 @@ import java.io.*;
 
 import java.nio.charset.Charset;
 import java.nio.file.*;
-        
-
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Niels Bang
  */
 public class Archive {
+
     private final String FILENAME = "archive.bin";
-    private final String orderArchive = "orderArchive.bin";        
+    private final String orderArchive = "orderArchive.bin";
     private final File orderFile = new File(orderArchive);
-    
+
     /*
     Creates the archive to store orders, customers and 
     
-    */
-    /*
+     */
+ /*
     public void createNewArchive(String fileName) {
         try{
         FileOutputStream fileOut = new FileOutputStream(fileName);
@@ -37,46 +35,46 @@ public class Archive {
         }
         
     }
-*/
+     */
     public void printReciept(int number) {
-        
+
     }
+
     /*
     public void addToArchive(){
         FileOutputStream out = new FileOutputStream()
         AppendObjectOutputStream objectAppend = new AppendObjectOutputStream();
     }
-    */
+     */
     public ArrayList<String> readArchive() {
         ArrayList<String> read = new ArrayList<>();
-        
+
         return read;
     }
-    
-        public ArrayList<Object> readOrders() {
+
+    public ArrayList<Object> readOrders() {
         ArrayList<Object> obj = new ArrayList<>();
         Object a = null;
         try {
             FileInputStream in = new FileInputStream(orderFile);
             ObjectInputStream sin = new ObjectInputStream(in);
-            while(true){
-                try{
+            while (true) {
+                try {
                     obj.add(sin.readObject());
-                } catch(EOFException ex){
+                } catch (EOFException ex) {
                     return obj;
                 }
             }
         } catch (FileNotFoundException ex) {
-                ex.printStackTrace();
-            
+            ex.printStackTrace();
+
         } catch (IOException | ClassNotFoundException ex) {
-                ex.printStackTrace();
-            
+            ex.printStackTrace();
+
         }
         return obj;
     }
-    
-    
+
     public void readNIO() {
         try {
             Charset cs = Charset.forName("UTF-8");
@@ -85,30 +83,30 @@ public class Archive {
             for (String line : lines) {
                 System.out.println(line);
             }
-        } catch (FileNotFoundException ex){
+        } catch (FileNotFoundException ex) {
             System.out.println("File not found.");
         } catch (IOException ex) {
-                ex.printStackTrace();
+            ex.printStackTrace();
         }
     }
     /*
     public Order findOrder(int orderNumber){
         
     }
-    */
-    
+     */
+
 }
 
 class AppendObjectOutputStream extends ObjectOutputStream {
 
-  public AppendObjectOutputStream(OutputStream out) throws IOException {
-    super(out);
-  }
+    public AppendObjectOutputStream(OutputStream out) throws IOException {
+        super(out);
+    }
 
-  @Override
-  protected void writeStreamHeader() throws IOException { //2tally not stolen from stackoverflow
+    @Override
+    protected void writeStreamHeader() throws IOException { //2tally not stolen from stackoverflow
 
-    reset();
-  }
-  
+        reset();
+    }
+
 }
